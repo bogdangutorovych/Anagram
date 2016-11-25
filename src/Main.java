500537f9e5c44aa916fdb36e5c39070fe5cc2e0b
Bogdan Gutorovych <bogdan.gutorovych@gmail.com>
2016-11-25 15:59:27 +0200

    Task 1 - version 1.0

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,43 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("This is a Task 1!");
-		System.out.println("Add message in 'dev'-branch ");
+		Scanner s = new Scanner(System.in);
+		System.out.println("Введи строку для преобразования: ");
+		String input_text = s.nextLine();
 
+		//делаем строку в которую будем выводить результат
+		String output_text = "";
+
+		//разбиваем начальную строку на слова
+		String[] words = input_text.split(" ");
+		for (String separate_word : words) {
+			String reverse_word_only_letters = "";
+
+			//из каждого слова получаем слово-реверс только с буквенными символами
+			char[] chars_of_each_word = separate_word.toCharArray();
+			for (int i = chars_of_each_word.length - 1; i >= 0; --i) {
+				if (Character.isAlphabetic(chars_of_each_word[i])) {
+					reverse_word_only_letters += chars_of_each_word[i];
+				}
+			}
+
+			//формируем выход сравнивая посимвольно начальное слово и слово-реверс с буквенными символами
+			char[] chars_of_each_word_only_letter = reverse_word_only_letters.toCharArray();
+			int j = 0;
+			for (int i = 0; i < chars_of_each_word.length; i++) {
+				if (Character.isAlphabetic(chars_of_each_word[i])) {
+					output_text += chars_of_each_word_only_letter[i-j];
+				} else {
+					j++; 
+					output_text += chars_of_each_word[i];
+				}
+			}
+			output_text += " ";
+		}
+		output_text = output_text.trim();
+		System.out.println("Результат:");
+		System.out.println(output_text);
 	}
 }
