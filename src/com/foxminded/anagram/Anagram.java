c3fe42faa13ae7adcf2fbe4ada570de5561a80d2
Bogdan Gutorovych <bogdan.gutorovych@gmail.com>
2016-11-26 13:32:48 +0200

    Task 1 version 2

--- src/com/foxminded/anagram/Anagram.java
+++ src/com/foxminded/anagram/Anagram.java
@@ -3,42 +3,42 @@ import java.util.Scanner;
 
 public class Anagram {
 
+	static String reverseWord(String word) {
+		String reverseWordOnlyLetters = "";
+		char[] wordChars = word.toCharArray();
+		for (int i = wordChars.length - 1; i >= 0; --i) {
+			if (Character.isAlphabetic(wordChars[i])) {
+				reverseWordOnlyLetters += wordChars[i];
+			}
+		}
+		return reverseWordOnlyLetters;
+	}
+
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		System.out.println("Введи строку для преобразования: ");
-		String input_text = s.nextLine();
+		System.out.println("Input initial text: ");
+		String inputText = s.nextLine();
 
-		//делаем строку в которую будем выводить результат
-		String output_text = "";
+		String outputText = "";
 
-		//разбиваем начальную строку на слова
-		String[] words = input_text.split(" ");
-		for (String separate_word : words) {
-			String reverse_word_only_letters = "";
+		String[] words = inputText.split(" ");
+		for (String separateWord : words) {
 
-			//из каждого слова получаем слово-реверс только с буквенными символами
-			char[] chars_of_each_word = separate_word.toCharArray();
-			for (int i = chars_of_each_word.length - 1; i >= 0; --i) {
-				if (Character.isAlphabetic(chars_of_each_word[i])) {
-					reverse_word_only_letters += chars_of_each_word[i];
-				}
-			}
-
-			//формируем выход сравнивая посимвольно начальное слово и слово-реверс с буквенными символами
-			char[] chars_of_each_word_only_letter = reverse_word_only_letters.toCharArray();
+			char[] wordChars = separateWord.toCharArray();
+			char[] wordLetterChars = reverseWord(separateWord).toCharArray();
 			int j = 0;
-			for (int i = 0; i < chars_of_each_word.length; i++) {
-				if (Character.isAlphabetic(chars_of_each_word[i])) {
-					output_text += chars_of_each_word_only_letter[i-j];
+			for (int i = 0; i < wordChars.length; i++) {
+				if (Character.isAlphabetic(wordChars[i])) {
+					outputText += wordLetterChars[i-j];
 				} else {
 					j++; 
-					output_text += chars_of_each_word[i];
+					outputText += wordChars[i];
 				}
 			}
-			output_text += " ";
+			outputText += " ";
 		}
-		output_text = output_text.trim();
-		System.out.println("Результат:");
-		System.out.println(output_text);
+		outputText = outputText.trim();
+		System.out.println("Converted text:");
+		System.out.println(outputText);
 	}
 }
