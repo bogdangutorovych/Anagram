9d27783d6cfdf731999b234b3d349c711f4b11fd
Bogdan Gutorovych <bogdan.gutorovych@gmail.com>
2016-11-29 12:50:08 +0200

    modyfied JUnit methtods

--- test/com/foxminded/anagram/AnagramTest.java
+++ test/com/foxminded/anagram/AnagramTest.java
@@ -9,14 +9,24 @@ public class AnagramTest {
 	Anagram anagram = new Anagram();
 
 	@Test
-	public void shouldReturnOnlyReverseLetterSymbols() {
-		assertEquals("Reverse word isn't correct", "Ba", anagram.reverseWord("a2!B3@"));
+	public void shouldReturnReverseWordLetterSymbolsOnly() {
+		assertEquals("Reverse word isn't correct", "", anagram.reverseWord(""));
+		assertEquals("Reverse word isn't correct", "", anagram.reverseWord("        "));
+		assertEquals("Reverse word isn't correct", "lkjhgfdsa", anagram.reverseWord("asdfghjkl"));
+		assertEquals("Reverse word isn't correct", "", anagram.reverseWord("1234567890"));
+		assertEquals("Reverse word isn't correct", "", anagram.reverseWord("!·$%&/()="));
+		assertEquals("Reverse word isn't correct", "Mnbvcxz", anagram.reverseWord("z1x2c3v4b5n6M7"));
 		assertEquals("Reverse word isn't correct", "afba", anagram.reverseWord("a2!b3@+f+=45a_)("));
 	}
 
 	@Test
-	public void shouldChangeOrderOfLetterSymbolsOnlyInEachWord(){
-		assertEquals("Modified text isn't correct", "dcba hgfe", anagram.reverseString("abcd efgh"));
+	public void shouldReverseStringAndChangeOrderOfLetterSymbolsOnlyInEachWord(){
+		assertEquals("Modified text isn't correct", "", anagram.reverseString(""));
+		assertEquals("Modified text isn't correct", "", anagram.reverseString("         "));
+		assertEquals("Modified text isn't correct", "rFeDwSqA tG iKuJyH", anagram.reverseString("AqSwDeFr Gt HyJuKi"));
+		assertEquals("Modified text isn't correct", "1234 56789 890", anagram.reverseString("1234 56789 890"));
+		assertEquals("Modified text isn't correct", "@#$ %^& *()", anagram.reverseString("@#$ %^& *()"));
+		assertEquals("Modified text isn't correct", "dc3ba hgf5e", anagram.reverseString("ab3cd efg5h"));
 		assertEquals("Modified text isn't correct", "d1cba hgf!e", anagram.reverseString("a1bcd efg!h"));
 	}
 
